f0264f1a7b044b0aac032d522b157a0a5cf41acf
RevisedJDUDE2 <JDUDEofficial134@gmail.com>
2023-12-19 17:35:50 +0800

    First

---
+++ Build/CMakeFiles/MinTextEditor.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MinTextEditor.dir/src/main.cpp.obj"
+  "CMakeFiles/MinTextEditor.dir/src/main.cpp.obj.d"
+  "MinTextEditor.exe"
+  "MinTextEditor.exe.manifest"
+  "MinTextEditor.pdb"
+  "libMinTextEditor.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MinTextEditor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <windows.h>
+
+int main() {
+  MessageBoxA(nullptr, "Hello World\n", "", MB_OK);
+  return 0;
+}
